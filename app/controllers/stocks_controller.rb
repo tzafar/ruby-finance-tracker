2bfe28fe5c735c506f1362030fd1c9f7abf44244
Toseef Zafar <mtouseef.zafar@gmail.com>
2021-03-06 13:44:27 +0000

    error handling for empty and wrong ticker symbol

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,17 @@
 class StocksController < ApplicationController
+
   def search
-    @stock = Stock.get_price(params[:stock])
-    render 'user/my_portfolio'
+    if params[:stock].present?
+      @stock = Stock.get_price(params[:stock])
+      if @stock
+        render 'user/my_portfolio'
+      else
+        flash[:alert] = 'Please enter a valid symbol'
+        redirect_to my_portfolio_url
+      end
+    else
+      flash[:alert] = 'Please enter a valid symbol'
+      redirect_to my_portfolio_url
+    end
   end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,7 +5,11 @@ class Stock < ApplicationRecord
       secret_token: Rails.application.credentials.iex[:secret_token],
                                   endpoint: 'https://sandbox.iexapis.com/v1')
 
-    client.price(ticker_symbol)
-    new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+    begin
+      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+    rescue => exception
+      nil
+    end
   end
+
 end
